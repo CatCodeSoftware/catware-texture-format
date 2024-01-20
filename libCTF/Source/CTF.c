08d2e72f919ddcbad798a963afe56182825c9317
PointThink <69512713+pointThink@users.noreply.github.com>
2024-01-20 15:22:51 +0100

    Buggy prototype

---
+++ libCTF/Source/CTF.c
@@ -0,0 +1,101 @@
+#include "CTF.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+CTF_Image* CTF_Image_Load( char* filePath )
+{
+	FILE* file = fopen( filePath, "rb" );
+	CTF_Image* image = malloc( sizeof( CTF_Image ) );
+
+	char header[4];
+	char* desiredHeader = "CTF";
+	u8 versionMajor, versionMinor;
+
+	fread( &header, 1, 4, file );
+
+	int diffrence = strcmp( header, desiredHeader );
+	if ( diffrence != 0 )
+		return 0;
+
+	fread( &versionMajor, 1, 1, file );
+	fread( &versionMinor, 1, 1, file );
+
+	if ( versionMajor == 1 )
+	{
+		if ( versionMinor == 0 )
+		{
+			fread( &image->width, 2, 1, file );
+			fread( &image->height, 2, 1, file );
+
+			fread( &image->scalingMethod, 1, 1, file );
+			fread( &image->wrapMode, 1, 1, file );
+
+			image->pixels = calloc( sizeof( u32 ),  image->width * image->height );
+			fread( image->pixels, sizeof( u32 ), image->width * image->height, file );
+
+			return image;
+		}
+
+		return 0;
+	}
+
+	return 0;
+
+	fclose( file );
+}
+
+CTF_Image* CTF_Image_Create( u32* pixels, u16 width, u16 height, CTF_ScalingMethod scalingMethod, CTF_WrapMode wrapMode )
+{
+	CTF_Image* image = malloc( sizeof( CTF_Image ) );
+
+	image->width = width;
+	image->height = height;
+	image->scalingMethod = scalingMethod;
+	image->wrapMode = wrapMode;
+
+	image->pixels = malloc( width * height * sizeof( u32 ) );
+	memcpy( image->pixels, pixels, height * width * sizeof( u32 ) );
+
+	return image;
+}
+
+void CTF_Image_Destroy( CTF_Image* image )
+{
+	free( image->pixels );
+	free( image );
+}
+
+void CTF_Image_WriteToDisk( char* path, CTF_Image* image )
+{
+	FILE* file = fopen( path, "wb" );
+
+	if ( file == NULL )
+		perror( "Error reading file: " );
+
+	// printf( "a" );
+
+	// header
+	fwrite( "CTF", 1, 4, file );
+
+	u8 versionMajor = 1;
+	u8 versionMinor = 0;
+
+	// version
+	fwrite( &versionMajor, 1, 1, file );
+	fwrite( &versionMinor, 1, 1, file );
+
+	// size
+	fwrite( &image->width, 2, 1, file );
+	fwrite( &image->height, 2, 1, file );
+
+	// render data
+	fwrite( &image->scalingMethod, 1, 1, file );
+	fwrite( &image->wrapMode, 1, 1, file );
+
+	// image data
+	fwrite( image->pixels, image->width * image->height, 1, file );
+
+	fclose( file );
+}
--- libCTF/Source/CTF.h
+++ libCTF/Source/CTF.h
@@ -1,1 +1,62 @@
 #pragma once
+
+#include <stdint.h>
+
+
+#if defined( LIBCTF_STATIC )
+#	define LIBCTF_API extern
+#elif defined( LIBCTF_SHARED )
+// todo: implement
+#else
+#	error Build type not defined
+#endif
+
+// int types
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int64_t i64;
+typedef uint64_t u64;
+
+#ifndef __cplusplus
+// boolean
+typedef enum
+{
+	false = 0,
+	true = 1,
+} bool;
+#endif
+
+#ifndef NULL
+#	define NULL 0
+#endif
+
+typedef enum
+{
+	NEAREST = 0, LINEAR = 1,
+} CTF_ScalingMethod;
+
+typedef enum
+{
+	STRECH = 0, WRAP = 1,
+} CTF_WrapMode;
+
+typedef struct
+{
+	u16 width;
+	u16 height;
+
+	CTF_ScalingMethod scalingMethod;
+	CTF_WrapMode wrapMode;
+
+	u32* pixels; // rgba
+} CTF_Image;
+
+CTF_Image* CTF_Image_Load( char* filePath );
+CTF_Image* CTF_Image_Create( u32* pixels, u16 width, u16 height, CTF_ScalingMethod scalingMethod, CTF_WrapMode wrapMode );
+void CTF_Image_Destroy( CTF_Image* image );
+
+void CTF_Image_WriteToDisk( char* path, CTF_Image* image );
